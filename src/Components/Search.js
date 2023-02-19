abf195aa671d1cd23a0b69388f4f164c09747634
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-19 14:22:36 +0530

    feat: search on Enter btn click functionality added

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,12 +2,20 @@ import React from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 
 const Search = ({ word, setWord, searchHandler }) => {
+  const onKeyPress = (e) => {
+    if (e.keyCode == 13) {
+      searchHandler();
+    } else {
+      return;
+    }
+  };
   return (
     <div className="max-w-3xl mx-auto my-2 ">
       <div className="flex relative">
         <input
           placeholder="Search your word..."
           value={word}
+          onKeyDown={(e) => onKeyPress(e)}
           onChange={(e) => setWord(e.target.value)}
           className="border bg-gray-100 text-black focus:outline-none  rounded-lg p-4 h-10 w-[100%]"
         />
