55feef19dfc44b6941b264c21f35122391515dcf
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-19 14:21:33 +0530

    feat: footer created

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,17 +6,23 @@ const Footer = () => {
     <div className="max-w-3xl mx-auto">
       <div className="flex justify-center gap-4  ">
         <div className="text-3xl cursor-pointer text-violet-500 ">
-          <BsGithub />
+          <a target={"_blank"} href={"https://github.com/gkumar-pandey"}>
+            <BsGithub />
+          </a>
         </div>
         <div className="text-3xl cursor-pointer text-violet-500 ">
-          <BsLinkedin />
+          <a target={"_blank"} href={"https://www.linkedin.com/in/gkpandey/"}>
+            <BsLinkedin />
+          </a>
         </div>
         <div className="text-3xl cursor-pointer text-violet-500 ">
-          <BsTwitter />
+          <a target={"_blank"} href={"https://twitter.com/gautamkp078"}>
+            <BsTwitter />
+          </a>
         </div>
       </div>
       <div className="text-center text-violet-500 font-semibold font-serif my-1">
-        Develop by <a>GKp</a>
+        Develop by <a>GKP</a>
       </div>
     </div>
   );
